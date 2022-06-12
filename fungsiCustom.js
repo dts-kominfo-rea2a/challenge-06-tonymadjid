38d43c8bc042ade352becf691795a3cca32748f8
tonymadjid <tonymadjid@yahoo.com>
2022-06-12 21:25:11 +0700

    tugas CallBack2

--- fungsiCustom.js
+++ fungsiCustom.js
@@ -1,29 +1,52 @@
 // TODO: import module bila dibutuhkan di sini
-
-// ! JANGAN DIMODIFIKASI
+const fs = require("fs")
+    // ! JANGAN DIMODIFIKASI
 let file1 = "./data1.json";
 let file2 = "./data2.json";
 let file3 = "./data3.json";
 
 // ! JANGAN DIMODIFIKASI
 let modifyFile1 = (val) => {
-  file1 = val;
+    file1 = val;
 };
 let modifyFile2 = (val) => {
-  file2 = val;
+    file2 = val;
 };
 let modifyFile3 = (val) => {
-  file3 = val;
+    file3 = val;
 };
 
 // TODO: Kerjakan bacaData
 // gunakan variabel file1, file2, dan file3
-const bacaData = null;
-
+const bacaData = (fnCallBack) => {
+    const list = [];
+    fs.readFile(file1, (err, data) => {
+        if (err) {
+            return console.log(err)
+        }
+        data = JSON.parse(data)
+        list.push(data.message.split(' ')[1])
+    })
+    fs.readFile(file2, (err, data) => {
+        if (err) {
+            return console.log(err)
+        }
+        data = JSON.parse(data)
+        list.push(data[0].message.split(' ')[1])
+    })
+    fs.readFile(file3, (err, data) => {
+        if (err) {
+            return console.log(err)
+        }
+        data = JSON.parse(data)
+        list.push(data[0].data.message.split(' ')[1])
+        fnCallBack(null, list)
+    })
+};
 // ! JANGAN DIMODIFIKASI
 module.exports = {
-  modifyFile1,
-  modifyFile2,
-  modifyFile3,
-  bacaData,
+    modifyFile1,
+    modifyFile2,
+    modifyFile3,
+    bacaData,
 };
